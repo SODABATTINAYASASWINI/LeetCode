c5f4b9666dd9b816c490ad62c24cca3478418e17
Yasaswini Sodabattina <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2025-04-05 19:33:33 +0530

    Time: 3 ms (39.68%), Space: 9.3 MB (42.82%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int check(vector<int>v){
-        int x = 0;
-        for(int i = 0; i < v.size(); i ++){
-            x ^= v[i];
-        }
-        return x;
-    }
+    // int check(vector<int>v){
+    //     int x = 0;
+    //     for(int i = 0; i < v.size(); i ++){
+    //         x ^= v[i];
+    //     }
+    //     return x;
+    // }
     int subsetXORSum(vector<int>& nums) {
-        // vector<vector<int>>v;
         int sum = 0;
         int s = pow(2,nums.size());
         for(int i = 0; i < s; i++){
-            vector<int>t;
+            // vector<int>t;
+            int x = 0;
             int n = i;
             for(int j = 0; j < nums.size(); j ++){
                 if(n & 1 == 1){
-                    t.push_back(nums[j]);
+                    // t.push_back(nums[j]);
+                    x ^= nums[j];
                 }
                 n = n >> 1;
             }
-            sum += check(t);
+            sum += x;
         }
         return sum;
     }
