7f43f2bed03a2ca66a121a92af4d47169b2ddb3e
Yasaswini Sodabattina <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2025-05-19 15:06:11 +0530

    Time: 0 ms (100%), Space: 26.7 MB (61.13%) - LeetHub

---
+++ 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int s = max(0, i - nums[i]);
+            for(int j = s; j <= i; j ++){
+                sum += nums[j];
+            }
+        }
+        return sum;
+    }
+};
