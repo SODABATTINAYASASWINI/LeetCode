74328ca0064997726c9562ead376893e1873a0c5
Yasaswini Sodabattina <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2025-06-09 12:00:34 +0530

    Time: 12 ms (10.18%), Space: 11.9 MB (29.57%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int>mp1;
+        map<char,int>mp2;
+        for(int i = 0; i < ransomNote.size(); i++){
+            mp1[ransomNote[i]] += 1;
+        }
+        for(int i = 0; i < magazine.size(); i++){
+            mp2[magazine[i]] += 1;
+        }
+        for(int i = 0; i < ransomNote.size(); i++){
+            if(mp2.find(ransomNote[i]) != mp2.end()){
+                if(mp1[ransomNote[i]] > mp2[ransomNote[i]]) return false;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
